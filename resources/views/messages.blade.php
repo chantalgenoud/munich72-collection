c8cd128d4b3e182313576ca78dc919ee20ac1146
chantalgenoud <chantal.genoud@gmail.com>
2022-06-14 11:45:52 +0200

    day 5 end homeoffice

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -11,6 +11,11 @@
 
    <!-- Styles -->
    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+   <!--Unser Stylesheet in public/css/styleheet-->
+   <link rel="stylesheet" href="/css/stylesheet.css">
+
+
   <!--Fonts Roboto-->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -22,10 +27,10 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-sm-10">
-      <h1>Munich 1972 Olympic Games Posters</h1>
+      <div class="col-sm-10 p-3">
+        <h1>Munich 1972 Olympic Games Posters</h1>
       </div>
-      <div class="col-sm-2">
+      <div class="col-sm-2 p-3">
         <img src="/img/main/m72-logo.png" alt="logo">
       </div>
     </div>
@@ -34,51 +39,23 @@
 
   <!--Here is the 1 row containing 2 pictures with padding according to mock-up-->
   <div class="container">
-  <div class="row" id="leftItem">
-    <div class="col-sm-6">
-        <img src="/img/main/m72-archery.png" alt="archery">
-    </div>
-    <div class="col-sm-6" id="rightItem">
-      <img src="/img/main/m72-basketball.png" alt="basketball">
-    </div>
-  </div>
-
-
-
-
-  <!--Here is the 2 row containing 2 pictures with padding according to mock-up, pictures must be adjusted-->
-  <div class="container">
-  <div class="row" id="leftItem">
-    <div class="col-sm-6">
-        <img src="/img/main/m72-archery.png" alt="archery">
-    </div>
-    <div class="col-sm-6" id="rightItem">
-      <img src="/img/main/m72-basketball.png" alt="basketball">
+    <div class="row">
+        <div class="col-sm-6">
+          <div class="leftItem">
+            <p>
+              <img src="/img/main/m72-archery.png" alt="archery">
+            Poster archery
+            </p>
+          </div>
+        </div>  
+  
+      <div class="col-sm-6">
+        <div class="rightItem">
+          <img src="/img/main/m72-basketball.png" alt="basketball">
+        <div>
+      </div>
     </div>
   </div>
-
-  <!--Here is the 3 row containing 2 pictures with padding according to mock-up, pictures must be adjusted-->
-  <div class="container">
-  <div class="row" id="leftItem">
-    <div class="col-sm-6">
-        <img src="/img/main/m72-archery.png" alt="archery">
-    </div>
-    <div class="col-sm-6" id="rightItem">
-      <img src="/img/main/m72-basketball.png" alt="basketball">
-    </div>
-  </div>
-
-  <!--Here is the 4 row containing 2 pictures with padding according to mock-up, pictures must be adjusted-->
-    <div class="container">
-  <div class="row" id="leftItem">
-    <div class="col-sm-6">
-        <img src="/img/main/m72-archery.png" alt="archery">
-    </div>
-    <div class="col-sm-6" id="rightItem">
-      <img src="/img/main/m72-basketball.png" alt="basketball">
-    </div>
-  </div>
-
 
     
 </body>
